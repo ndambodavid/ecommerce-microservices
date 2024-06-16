5be9c4179b0f05da012d2d1df9cfbce73a04b1f8
ndambodavid <linmik420@gmail.com>
2024-06-16 16:11:11 +0300

    Product and category ; product repository implementation

---
+++ product/src/main/java/com/ndambodevs/product/model/Category.java
@@ -0,0 +1,23 @@
+package com.ndambodevs.product.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+
+}
